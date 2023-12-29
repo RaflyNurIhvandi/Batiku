6a8ef7d335ee42145e74d279491603138cefaae5
Rafly Nur Ihvandi <rafliamad620@gmail.com>
2023-12-29 10:33:41 +0700

    responsif

---
+++ public/js/script.js
@@ -0,0 +1,12 @@
+function dis_none1() {
+    document.getElementById('nav-dr-cn1').style.display = 'none'
+}
+function dis_block1() {
+    document.getElementById('nav-dr-cn1').style.display = 'block'
+}
+function dis_none2() {
+    document.getElementById('nav-dr-cn2').style.display = 'none'
+}
+function dis_block2() {
+    document.getElementById('nav-dr-cn2').style.display = 'block'
+}
